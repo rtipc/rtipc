a28aaf61dc3a6af4d8c6a465313c140d8d498ca4
Simon Maurer <mail@maurer.systems>
2026-09-17 16:57:38 +0200

    update CPP wrapper

--- include/rtipc/rtipc.hpp
+++ include/rtipc/rtipc.hpp
@@ -6,6 +6,7 @@
 #include <span>
 #include <stdexcept>
 #include <tuple>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -16,6 +17,39 @@ struct ri_group;
 }
 
 namespace rtipc {
+
+template <typename T>
+concept TriviallyCopyable = std::is_trivially_copyable_v<T>;
+
+void consumer_release(::ri_consumer *consumer);
+void producer_release(::ri_producer *producer);
+void group_delete(::ri_group *group);
+
+struct ConsumerDeleter {
+  void operator()(ri_consumer *consumer) const noexcept {
+    if (consumer)
+      consumer_release(consumer);
+  }
+};
+
+struct ProducerDeleter {
+  void operator()(ri_producer *producer) const noexcept {
+    if (producer)
+      producer_release(producer);
+  }
+};
+
+struct GroupDeleter {
+  void operator()(ri_group *group) const noexcept {
+    if (group)
+      group_delete(group);
+  }
+};
+
+using ConsumerPtr = std::unique_ptr<ri_consumer, ConsumerDeleter>;
+using ProducerPtr = std::unique_ptr<ri_producer, ProducerDeleter>;
+using GroupPtr = std::unique_ptr<ri_group, GroupDeleter>;
+using Info = std::vector<char>;
 
 enum class QueueError {
   invalid_index,
@@ -42,45 +76,14 @@ struct ChannelAttr {
   size_t message_size;
   unsigned additional_messages;
   bool fd;
-  std::vector<std::byte> info;
+  Info info;
 };
 
 struct GroupAttr {
   std::vector<ChannelAttr> consumers;
   std::vector<ChannelAttr> producers;
-  std::vector<std::byte> info;
+  Info info;
 };
-
-
-void consumer_release(ri_consumer* consumer);
-void producer_release(ri_producer* producer);
-void group_delete(ri_group* group);
-
-
-struct ConsumerDeleter {
-  void operator()(ri_consumer* consumer) const noexcept {
-    if (consumer)
-      consumer_release(consumer);
-  }
-};
-
-struct ProducerDeleter {
-  void operator()(ri_producer* producer) const noexcept {
-    if (producer)
-      producer_release(producer);
-  }
-};
-
-struct GroupDeleter {
-  void operator()(ri_group* group) const noexcept {
-    if (group)
-      group_delete(group);
-  }
-};
-
-using ConsumerPtr = std::unique_ptr<ri_consumer, ConsumerDeleter>;
-using ProducerPtr = std::unique_ptr<ri_producer, ProducerDeleter>;
-using GroupPtr = std::unique_ptr<ri_group, GroupDeleter>;
 
 class ConsumerBase {
   friend class ChannelGroup;
@@ -91,11 +94,11 @@ protected:
 public:
   virtual ~ConsumerBase() noexcept = default;
 
-  ConsumerBase(const ConsumerBase&) = delete;
-  ConsumerBase& operator=(const ConsumerBase&) = delete;
+  ConsumerBase(const ConsumerBase &) = delete;
+  ConsumerBase &operator=(const ConsumerBase &) = delete;
 
-  ConsumerBase(ConsumerBase&& other) noexcept = default;
-  ConsumerBase& operator=(ConsumerBase&& other) noexcept = default;
+  ConsumerBase(ConsumerBase &&other) noexcept = default;
+  ConsumerBase &operator=(ConsumerBase &&other) noexcept = default;
 
   std::expected<PopResult, QueueError> pop() noexcept;
   std::expected<unsigned, QueueError> count_messages() const noexcept;
@@ -104,8 +107,7 @@ protected:
   ConsumerPtr consumer_;
 };
 
-template <typename T>
-class Consumer final : private ConsumerBase {
+template <TriviallyCopyable T> class Consumer final : private ConsumerBase {
   friend class ChannelGroup;
 
 private:
@@ -117,14 +119,14 @@ public:
   ~Consumer() noexcept override = default;
 
   // Non-copyable
-  Consumer(const Consumer&) = delete;
-  Consumer& operator=(const Consumer&) = delete;
+  Consumer(const Consumer &) = delete;
+  Consumer &operator=(const Consumer &) = delete;
 
   // Movable
-  Consumer(Consumer&& other) noexcept = default;
-  Consumer& operator=(Consumer&& other) noexcept = default;
+  Consumer(Consumer &&other) noexcept = default;
+  Consumer &operator=(Consumer &&other) noexcept = default;
 
-  std::optional<const T&> current_message() const noexcept;
+  std::optional<const T &> current_message() const noexcept;
 
   using ConsumerBase::count_messages;
   using ConsumerBase::pop;
@@ -140,12 +142,12 @@ public:
   virtual ~ProducerBase() noexcept = default;
 
   // Non-copyable
-  ProducerBase(const ProducerBase&) = delete;
-  ProducerBase& operator=(const ProducerBase&) = delete;
+  ProducerBase(const ProducerBase &) = delete;
+  ProducerBase &operator=(const ProducerBase &) = delete;
 
   // Movable
-  ProducerBase(ProducerBase&& other) noexcept = default;
-  ProducerBase& operator=(ProducerBase&& other) noexcept = default;
+  ProducerBase(ProducerBase &&other) noexcept = default;
+  ProducerBase &operator=(ProducerBase &&other) noexcept = default;
 
   std::expected<TryPushResult, QueueError> try_push() noexcept;
   std::expected<ForcePushResult, QueueError> force_push() noexcept;
@@ -158,8 +160,7 @@ protected:
   ProducerPtr producer_;
 };
 
-template <typename T>
-class Producer final : protected ProducerBase {
+template <TriviallyCopyable T> class Producer final : protected ProducerBase {
   friend class ChannelGroup;
 
 private:
@@ -170,12 +171,12 @@ public:
   ~Producer() noexcept override = default;
 
   // Non-copyable
-  Producer(const Producer&) = delete;
-  Producer& operator=(const Producer&) = delete;
+  Producer(const Producer &) = delete;
+  Producer &operator=(const Producer &) = delete;
 
   // Movable
-  Producer(Producer&& other) noexcept = default;
-  Producer& operator=(Producer&& other) noexcept = default;
+  Producer(Producer &&other) noexcept = default;
+  Producer &operator=(Producer &&other) noexcept = default;
 
   using ProducerBase::cache_disable;
   using ProducerBase::cache_enable;
@@ -183,47 +184,45 @@ public:
   using ProducerBase::force_push;
   using ProducerBase::try_push;
 
-  T& current_message() noexcept;
+  T &current_message() noexcept;
 };
 
 class ChannelGroup final {
 public:
   // deserializw
-  explicit ChannelGroup(const std::span<std::byte>& req,
-                        std::span<int>& fds);
+  explicit ChannelGroup(const std::span<std::byte> &req, std::span<int> &fds);
 
-  explicit ChannelGroup(const GroupAttr& attr);
+  explicit ChannelGroup(const GroupAttr &attr);
 
   ~ChannelGroup() noexcept = default;
 
   // Non-copyable
-  ChannelGroup(const ChannelGroup&) = delete;
-  ChannelGroup& operator=(const ChannelGroup&) = delete;
+  ChannelGroup(const ChannelGroup &) = delete;
+  ChannelGroup &operator=(const ChannelGroup &) = delete;
 
   // Movable
-  ChannelGroup(ChannelGroup&& other) noexcept = default;
-  ChannelGroup& operator=(ChannelGroup&& other) noexcept = default;
-
+  ChannelGroup(ChannelGroup &&other) noexcept = default;
+  ChannelGroup &operator=(ChannelGroup &&other) noexcept = default;
 
   std::tuple<std::vector<std::byte>, std::vector<int>> serialize();
 
-  template <typename T>
-  Consumer<T> acquire_consumer(unsigned index) {
+  template <TriviallyCopyable T> Consumer<T> acquire_consumer(unsigned index) {
+
     size_t size = consumer_message_size(index);
 
     if (size < sizeof(T))
-      throw std::length_error(
-          "channel message size too small to hold struct");
+      throw std::length_error("channel message size too small to hold struct");
 
     return Consumer<T>(acquire_consumer_impl(index));
   }
 
-  template <typename T>
-  Producer<T> acquire_producer(unsigned index) {
+  template <TriviallyCopyable T> Producer<T> acquire_producer(unsigned index) {
+    if (!std::is_trivially_copyable_v<T>)
+      throw std::invalid_argument("Message type is not trivially copyable");
+
     size_t size = producer_message_size(index);
     if (size < sizeof(T))
-      throw std::length_error(
-          "channel message size too small to hold struct");
+      throw std::length_error("channel message size too small to hold struct");
 
     return Producer<T>(acquire_producer_impl(index));
   }
--- src/rtipc.cpp
+++ src/rtipc.cpp
@@ -2,31 +2,70 @@
 
 #include <rtipc/rtipc.h>
 
-using namespace rtipc;
+namespace rtipc {
 
-void consumer_release(ri_consumer* consumer)
-{
+void consumer_release(::ri_consumer *consumer) {
   if (consumer)
-    ri_consumer_release(consumer);
+    ::ri_consumer_release(consumer);
 }
 
-
-void producer_release(ri_producer* producer)
-{
+void producer_release(::ri_producer *producer) {
   if (producer)
-    ri_producer_release(producer);
+    ::ri_producer_release(producer);
 }
 
-
-void group_delete(ri_group* group)
-{
+void group_delete(::ri_group *group) {
   if (group)
-    ri_group_delete(group);
+    ::ri_group_delete(group);
 }
 
+static constexpr ::ri_info_t to_c_info(const Info &info) {
+  return ::ri_info_t{.size = info.size(), .data = info.data()};
+}
 
-ConsumerPtr ChannelGroup::acquire_consumer_impl(unsigned index)
-{
+static constexpr ::ri_channel_attr_t
+to_c_channel_attr(const ChannelAttr &attr) {
+  return ::ri_channel_attr_t{.msg_size = attr.message_size,
+                             .add_msgs = attr.additional_messages,
+                             .eventfd = attr.fd,
+                             .info = to_c_info(attr.info)};
+}
+
+static const ::ri_channel_attr_t zero_attr{};
+
+ChannelGroup::ChannelGroup(const GroupAttr &group_attr) {
+  std::vector<ri_channel_attr_t> c_consumers;
+  std::vector<ri_channel_attr_t> c_producers;
+
+  c_consumers.reserve(group_attr.consumers.size() + 1);
+  c_producers.reserve(group_attr.producers.size() + 1);
+
+  for (const auto &attr : group_attr.consumers) {
+    c_consumers.emplace_back(to_c_channel_attr(attr));
+  }
+
+  c_consumers.emplace_back(zero_attr);
+
+  for (const auto &attr : group_attr.producers) {
+    c_producers.emplace_back(to_c_channel_attr(attr));
+  }
+
+  c_producers.emplace_back(zero_attr);
+
+  ::ri_group_attr_t c_group_attr = {.consumers = c_consumers.data(),
+                                    .producers = c_producers.data(),
+                                    .info = to_c_info(group_attr.info)};
+
+  ::ri_group_t *group = ::ri_group_from_attr(&c_group_attr);
+
+  if (group == nullptr) {
+    throw std::runtime_error("ri_group_from_attr returned NULL");
+  }
+
+  group_ = GroupPtr(group);
+}
+
+ConsumerPtr ChannelGroup::acquire_consumer_impl(unsigned index) {
   ri_consumer *consumer = ::ri_group_acquire_consumer(group_.get(), index);
 
   if (consumer == nullptr)
@@ -35,9 +74,7 @@ ConsumerPtr ChannelGroup::acquire_consumer_impl(unsigned index)
   return ConsumerPtr(consumer);
 }
 
-
-ProducerPtr ChannelGroup::acquire_producer_impl(unsigned index)
-{
+ProducerPtr ChannelGroup::acquire_producer_impl(unsigned index) {
   ri_producer *producer = ::ri_group_acquire_producer(group_.get(), index);
 
   if (producer == nullptr)
@@ -46,24 +83,21 @@ ProducerPtr ChannelGroup::acquire_producer_impl(unsigned index)
   return ProducerPtr(producer);
 }
 
-
-size_t ChannelGroup::consumer_message_size(unsigned index) const
-{
-  const ri_channel_attr_t *attr = ::ri_group_get_consumer_attr(group_.get(), index);
+size_t ChannelGroup::consumer_message_size(unsigned index) const {
+  const ri_channel_attr_t *attr =
+      ::ri_group_get_consumer_attr(group_.get(), index);
   if (attr == nullptr)
     throw std::runtime_error("ri_group_get_consumer_attr returned NULL");
 
   return attr->msg_size;
 }
 
-
-size_t ChannelGroup::producer_message_size(unsigned index) const
-{
-  const ri_channel_attr_t *attr = ::ri_group_get_producer_attr(group_.get(), index);
+size_t ChannelGroup::producer_message_size(unsigned index) const {
+  const ri_channel_attr_t *attr =
+      ::ri_group_get_producer_attr(group_.get(), index);
   if (attr == nullptr)
     throw std::runtime_error("ri_group_get_producer_attr returned NULL");
 
   return attr->msg_size;
 }
-
-
+} // namespace rtipc
