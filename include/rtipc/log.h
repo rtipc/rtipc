9094c5e0b398da90a16606428a7ccd08e7e216f7
Simon Maurer <mail@maurer.systems>
2026-09-18 14:06:19 +0200

    improve log.h

--- include/rtipc/log.h
+++ include/rtipc/log.h
@@ -4,42 +4,71 @@
 extern "C" {
 #endif
 
+#define RI_UNUSED(x) ((void)(x))
 
-
+#define RI_LOG_LEVEL_NONE 0
+#define RI_LOG_LEVEL_ERR 1
+#define RI_LOG_LEVEL_WRN 2
+#define RI_LOG_LEVEL_INF 3
+#define RI_LOG_LEVEL_DBG 4
 
 #if RTIPC_LOG_DISABLE == 1
+#define RI_LOG_MAX_LEVEL RI_LOG_LEVEL_NONE
+#else
+#ifndef RI_LOG_MAX_LEVEL
+#define RI_LOG_MAX_LEVEL RI_LOG_LEVEL_ERR
+#endif
+#endif
 
-#define LOG_ERR(format, ...) (void) 0
-#define LOG_WRN(format, ...)  (void) 0
-#define LOG_INF(format, ...)  (void) 0
-#define LOG_DBG(format, ...)  (void) 0
+#define RI_NO_LOG(format, ...) ri_no_log(format, ##__VA_ARGS__)
 
-#else /* RTIPC_LOG_DISABLE == 0 */
+#define RI_LOG_STRINGIFY(val) RI_LOG_STRINGIFY_ARG(val)
+#define RI_LOG_STRINGIFY_ARG(contents) #contents
 
-#define LOG_STRINGIFY(val) LOG_STRINGIFY_ARG(val)
-#define LOG_STRINGIFY_ARG(contents) #contents
+#define RI_LOG(priority, format, ...)                                          \
+  ri_log(priority, __FILE_NAME__, RI_LOG_STRINGIFY(__LINE__), __func__,        \
+         format "\n", ##__VA_ARGS__)
 
-#define LOG_LEVEL_NONE 0
-#define LOG_LEVEL_ERR  1
-#define LOG_LEVEL_WRN  2
-#define LOG_LEVEL_INF  3
-#define LOG_LEVEL_DBG  4
+#if RI_LOG_MAX_LEVEL == RI_LOG_LEVEL_NONE
 
-#define RI_LOG(priority, format, ...)  ri_log(priority, \
-                                        __FILE_NAME__, LOG_STRINGIFY(__LINE__), \
-                                        __func__, format "\n", ##__VA_ARGS__)
+#define LOG_ERR(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#define LOG_WRN(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#define LOG_INF(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#define LOG_DBG(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
 
-#define LOG_ERR(format, ...)  RI_LOG(LOG_LEVEL_ERR, format, ##__VA_ARGS__)
-#define LOG_WRN(format, ...)   RI_LOG(LOG_LEVEL_WRN, format, ##__VA_ARGS__)
-#define LOG_INF(format, ...)   RI_LOG(LOG_LEVEL_INF, format, ##__VA_ARGS__)
-#define LOG_DBG(format, ...)  RI_LOG(LOG_LEVEL_DBG, format, ##__VA_ARGS__)
+#else /* RI_LOG_MAX_LEVEL == RI_LOG_LEVEL_NONE */
 
+#if RI_LOG_MAX_LEVEL == RI_LOG_LEVEL_ERR
+#define LOG_ERR(format, ...) RI_LOG(RI_LOG_LEVEL_ERR, format, ##__VA_ARGS__)
+#define LOG_WRN(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#define LOG_INF(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#define LOG_DBG(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#elif RI_LOG_MAX_LEVEL == RI_LOG_LEVEL_WRN
+#define LOG_ERR(format, ...) RI_LOG(RI_LOG_LEVEL_ERR, format, ##__VA_ARGS__)
+#define LOG_WRN(format, ...) RI_LOG(RI_LOG_LEVEL_WRN, format, ##__VA_ARGS__)
+#define LOG_INF(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#define LOG_DBG(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#elif RI_LOG_MAX_LEVEL == RI_LOG_LEVEL_INF
+#define LOG_ERR(format, ...) RI_LOG(RI_LOG_LEVEL_ERR, format, ##__VA_ARGS__)
+#define LOG_WRN(format, ...) RI_LOG(RI_LOG_LEVEL_WRN, format, ##__VA_ARGS__)
+#define LOG_INF(format, ...) RI_LOG(RI_LOG_LEVEL_INF, format, ##__VA_ARGS__)
+#define LOG_DBG(format, ...) RI_NO_LOG(format, ##__VA_ARGS__)
+#elif RI_LOG_MAX_LEVEL == RI_LOG_LEVEL_DBG
+#define LOG_ERR(format, ...) RI_LOG(RI_LOG_LEVEL_ERR, format, ##__VA_ARGS__)
+#define LOG_WRN(format, ...) RI_LOG(RI_LOG_LEVEL_WRN, format, ##__VA_ARGS__)
+#define LOG_INF(format, ...) RI_LOG(RI_LOG_LEVEL_INF, format, ##__VA_ARGS__)
+#define LOG_DBG(format, ...) RI_LOG(RI_LOG_LEVEL_DBG, format, ##__VA_ARGS__)
+#endif
 
-void ri_log(
-    int priority, const char *file, const char *line, const char *func, const char *format, ...)
-    __attribute__((format(printf, 5, 6)));
+#endif /* RI_LOG_MAX_LEVEL == RI_LOG_LEVEL_NONE */
 
-#endif /* RTIPC_LOG_DISABLE */
+void ri_log(int priority, const char *file, const char *line, const char *func,
+            const char *format, ...) __attribute__((format(printf, 5, 6)));
+
+static inline void ri_no_log(const char *format, ...)
+    __attribute__((format(printf, 1, 2)));
+
+static inline void ri_no_log(const char *format, ...) { RI_UNUSED(format); }
 
 #ifdef __cplusplus
 }
